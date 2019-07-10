8258d3c5d7fff3dec34e28f1bf138ecdbcfb55bd
phoenix fate <sm516116978@outlook.com>
2019-07-10 16:14:43 +0800

    day06 finished

---
+++ day06-webpack-vue-children-router/src/subcomponents/login.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h3>这是account的登录子组件</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+    div {
+        color:red;
+    }
+</style>
+
+
+
